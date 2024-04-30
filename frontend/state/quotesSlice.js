232e460f0ab8a5741579fe8adbe970c0353d8a81
brightgirlkell <teshara2210@icloud.com>
2024-04-30 14:26:03 -0400

    updated files

--- frontend/state/quotesSlice.js
+++ frontend/state/quotesSlice.js
@@ -1,7 +1,7 @@
-// ✨ create your `quotesSlice` in this module
+import { createSlice } from "@reduxjs/toolkit"// ✨ create your `quotesSlice` in this module
 
 let id = 1
-const getNextId = () => id++
+export const getNextId = () => id++
 const initialState = {
   displayAllQuotes: true,
   highlightedQuote: null,
@@ -10,7 +10,7 @@ const initialState = {
       id: getNextId(),
       quoteText: "Don't cry because it's over, smile because it happened.",
       authorName: "Dr. Seuss",
-      apocryphal: false,
+      apocryphal: true,
     },
     {
       id: getNextId(),
@@ -26,3 +26,41 @@ const initialState = {
     },
   ],
 }
+
+export const quoteSlice = createSlice({
+  name:'quotes',
+  initialState,
+  reducers:{
+    toggleVisibility(state){
+    state.displayAllQuotes= !state.displayAllQuotes
+    },
+    deleteQuote(state,action){
+   state.quotes = state.quotes.filter(qt =>qt.id !== action.payload)
+    },
+    editQuoteAuthenticity(state,action){
+    const quoteToEdit = state.quotes.find(qt => qt.id === action.payload)
+    quoteToEdit.apocryphal = !quoteToEdit.apocryphal
+    },
+    setHighlightedQuote(state,action){
+     if(state.highlightedQuote === action.payload){
+      state.highlightedQuote = null
+     } else {
+      state.highlightedQuote = action.payload
+     }
+     
+    },
+    createQuote(state,action){
+     state.quotes.push(action.payload)
+    }
+  }
+})
+
+export const {
+  createQuote,
+  deleteQuote,
+  editQuoteAuthenticity,
+  setHighlightedQuote,
+  toggleVisibility,
+}= quoteSlice.actions
+
+export default quoteSlice.reducer
--- frontend/state/store.js
+++ frontend/state/store.js
@@ -1,1 +1,9 @@
+import { configureStore } from "@reduxjs/toolkit";
+import quotesReducer from './quotesSlice'
+
+export const store = configureStore({
+    reducer:{
+        quotesState:quotesReducer,
+    }
+})
 // ✨ create your `store` in this module
